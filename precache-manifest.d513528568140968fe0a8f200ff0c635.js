6781eb6df0f6aed5a1571e37f6c16931a286c8dc
GLaDOS <support@glados.im>
2019-10-31 21:44:48 +0800

    first commit

---
+++ precache-manifest.d513528568140968fe0a8f200ff0c635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34098d2efa2ea40ecfb7040ea45d453c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a93a0dc0df15775415b2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "80316ff71f848ec413ae",
+    "url": "/static/js/2.dc9d1f8b.chunk.js"
+  },
+  {
+    "revision": "a93a0dc0df15775415b2",
+    "url": "/static/js/main.4394f7a4.chunk.js"
+  },
+  {
+    "revision": "ed9cbb36699891babaa8",
+    "url": "/static/js/runtime-main.d7d43982.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
